e656dd0c6fe172f7c847f39e397a218c6619529e
Anthony Greco <anthony.greco@freestar.io>
2021-02-04 11:35:09 -0500

    attempting to add rllup build to vue component

---
+++ src/component-wrapper.js
@@ -0,0 +1,22 @@
+import component from './components/freestarAdSlot.vue'
+
+export function install(Vue) {
+	if (install.installed) return
+	install.installed = true
+	Vue.component('FreestarAdSlot', component)
+}
+
+const plugin = { install }
+
+let GlobalVue = null
+if (typeof window !== 'undefined') {
+	GlobalVue = window.Vue
+} else if (typeof global !== 'undefined') {
+	GlobalVue = global.Vue
+}
+
+if (GlobalVue) {
+	GlobalVue.use(plugin)
+}
+
+export default component
